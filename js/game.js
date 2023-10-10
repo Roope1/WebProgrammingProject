56679b6215b23a0c55feb017428fc582f3002202
Roope Turkki <roopeh.turkki@gmail.com>
2023-10-10 19:57:43 +0300

    animations added but theyre quite funky still

--- js/game.js
+++ js/game.js
@@ -32,7 +32,7 @@ class Main extends Phaser.Scene {
 
     preload() {
         this.load.image("groundBlock", "./assets/images/Ground_SMB.png") // TODO: change to something copyright free
-        this.load.image("player", "./assets/images/8Bit_Mario.webp")
+        this.load.spritesheet("player", "./assets/sprites/player_sprite_fixed.png", {frameWidth: 35, frameHeight: 60})
     }
 
     create() {
@@ -51,14 +51,30 @@ class Main extends Phaser.Scene {
         }
 
         // adding player to the scene
-        this.player = this.physics.add.image(game.config.width / 2, game.config.height / 2, "player")
-        this.player.scale = 0.1
+        this.player = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "player")
+       
         this.player.body.gravity.y = gameOptions.gravity
 
         this.physics.add.collider(this.player, this.groundGroup)
 
         // get keyboard inputs
         this.cursors = this.input.keyboard.createCursorKeys()
+
+        // playerAnimations
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("player", {start: 0, end: 1}),
+        })
+
+        this.anims.create({
+            key:"right",
+            frames: this.anims.generateFrameNumbers("player", {start: 3, end: 4})
+        })
+
+        this.anims.create({
+            key: "still",
+            frames: [{key: "player", frame: 2}],
+        })
 
     }
 
@@ -71,8 +87,12 @@ class Main extends Phaser.Scene {
         let moveDir = 0;
         if (this.cursors.left.isDown) {
             moveDir = 1;
+            this.player.anims.play("left", true)
         } else if (this.cursors.right.isDown) {
             moveDir = -1;
+            this.player.anims.play("right", true)
+        } else {
+            this.player.anims.play("still", true)
         }
         
         // HACK: moving ground instead of making level scrollable (maybe FIXME?)
