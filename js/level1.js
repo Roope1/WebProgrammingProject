a2c2616b36879de605bfd1f96779403c91352f57
Roope Turkki <roopeh.turkki@gmail.com>
2023-10-14 12:14:28 +0300

    enemy added, player can kill enemy or die

--- js/level1.js
+++ js/level1.js
@@ -1,14 +1,23 @@
 class Level1 extends Phaser.Scene {
 
+    constructor() {
+        super("Level1")
+    }
+
     preload() {
         this.load.image("groundBlock", "./assets/images/Ground_SMB.png") // TODO: change to something copyright free
         this.load.spritesheet("player", "./assets/sprites/player_sprite.png", {frameWidth: 35, frameHeight: 60})
+        this.load.image("enemy", "./assets/images/goomba.jpg")
     }
 
     create() {
         this.groundGroup = this.physics.add.group({
             immovable: true,
             allowGravity: false,
+        })
+
+        this.enemyGroup = this.physics.add.group({
+            allowGravity: true
         })
 
         // creating the ground layer
@@ -24,12 +33,22 @@ class Level1 extends Phaser.Scene {
         // TODO: different texture for these
         addPlatform(3, 300, game.config.height / 2, "groundBlock", this)
 
+        // adding enemies
+        let enemy = this.enemyGroup.create(800, game.config.height / 2, "enemy")
+        enemy.scale = 0.2
+        // adding gravity to enemies 
+        enemy.setGravityY(gameOptions.gravity)
+
+        // colliders between enemy and ground
+        this.physics.add.collider(this.enemyGroup, this.groundGroup)
+        
         // adding player to the scene
         this.player = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, "player")
-       
+        
         this.player.body.gravity.y = gameOptions.gravity
-
+        
         this.physics.add.collider(this.player, this.groundGroup)
+        this.physics.add.overlap(this.player, this.enemyGroup, this.checkHit, null, this)
 
         // get keyboard inputs
         this.cursors = this.input.keyboard.createCursorKeys()
@@ -78,8 +97,22 @@ class Level1 extends Phaser.Scene {
             this.player.body.velocity.y = - gameOptions.jumpForce;
         }
 
+
+        // enemies should always be moving left
+        this.enemyGroup.setVelocityX(-100)
+
     }
 
+    checkHit(player, enemy) {
+        // player needs to hit the enemy from top to kill enemy, otherwise player dies
+        if (enemy.body.touching.up) {
+            console.log("enemy died")
+            enemy.disableBody(true, true)
+        } else {
+            this.scene.start("Level1")
+        }
+
+    }
    
 
 }
