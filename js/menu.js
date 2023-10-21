462e232beffa002c19e717fe95787ad5ded8fdc3
Roope Turkki <roopeh.turkki@gmail.com>
2023-10-21 17:55:07 +0300

    menu done

--- js/menu.js
+++ js/menu.js
@@ -7,10 +7,29 @@ class Menu extends Phaser.Scene {
     preload() {
         this.load.image("volumeOn", "./assets/images/volumeOn.png")
         this.load.image("volumeOff", "./assets/images/volumeOff.png")
+        this.load.image("tauno", "./assets/images/tauno.png")
     }
 
     create() {
-        const level1Btn = this.add.text(100, 100, "Play", { fill: "#fff"})
+        // title stuff
+        this.titleStyle = {
+            fontFamily: "Verdana",
+            fontSize: "68px"
+        }
+
+        this.add.text(32, 32, "Taunos adventure", this.titleStyle)
+
+        // image of tauno
+        this.character = this.add.image(100, 200, "tauno")
+        this.character.scale = 2
+
+        // play button style
+        this.playButtonStyle = {
+            fontFamily: "Verdana",
+            fontSize: "40px"
+        }
+
+        const level1Btn = this.add.text(game.config.width / 3, game.config.height / 1.3, "Start Game", this.playButtonStyle)
         level1Btn.setInteractive();
 
         level1Btn.on('pointerup', () => {
@@ -18,13 +37,6 @@ class Menu extends Phaser.Scene {
             this.scene.start("Level1")
         })
         
-        const level2Btn = this.add.text(100, 150, "Play2", { fill: "#fff"})
-        level2Btn.setInteractive();
-
-        level2Btn.on('pointerup', () => {
-            console.log("Loading level 2")
-            this.scene.start("Level2")
-        })
         const volume = this.add.image(game.config.width - 50, game.config.height - 50, "volumeOn")
         volume.scale = 0.1
         volume.setInteractive();
